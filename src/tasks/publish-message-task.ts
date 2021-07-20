0d2575d39d0ae1ffdf08681b52e516c5ad60d5d1
Alexandre Chau <accounts@chau.moe>
2021-07-20 15:29:58 +0200

    feat: call publish message hooks

--- src/tasks/publish-message-task.ts
+++ src/tasks/publish-message-task.ts
@@ -1,3 +1,4 @@
+import { FastifyLoggerInstance } from 'fastify';
 import {
   DatabaseTransactionHandler,
   ItemMembershipService,
@@ -30,12 +31,17 @@ export class PublishMessageTask extends BaseChatTask<ChatMessage> {
     this.data = data;
   }
 
-  async run(handler: DatabaseTransactionHandler): Promise<void> {
+  async run(
+    handler: DatabaseTransactionHandler,
+    log: FastifyLoggerInstance,
+  ): Promise<void> {
     this.status = 'RUNNING';
 
     // get item for which we're fetching the chat
     const item = await this.itemService.get(this.targetId, handler);
-    if (!item) { throw new ItemNotFound(this.targetId); }
+    if (!item) {
+      throw new ItemNotFound(this.targetId);
+    }
 
     // verify if member has access to this chat
     const hasRights = await this.itemMembershipService.canRead(
@@ -43,13 +49,18 @@ export class PublishMessageTask extends BaseChatTask<ChatMessage> {
       item,
       handler,
     );
-    if (!hasRights) { throw new MemberCannotReadItem(this.targetId); }
+    if (!hasRights) {
+      throw new MemberCannotReadItem(this.targetId);
+    }
 
     // set chatId and author
     this.data.chatId = item.id;
     this.data.creator = this.actor.id;
+
     // publish message
+    await this.preHookHandler?.(this.data, this.actor, { log, handler });
     const res = await this.chatService.publishMessage(this.data, handler);
+    await this.postHookHandler?.(res, this.actor, { log, handler });
 
     // return chat message
     this._result = res;
