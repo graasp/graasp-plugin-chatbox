37eb7efb236c10e14cc04736a23bb3595a98ded9
Alexandre Chau <accounts@chau.moe>
2021-07-22 15:53:09 +0200

    build: export service and task manager

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import plugin from './service-api';
 
+export * from './db-service';
 export * from './interfaces/chat';
 export * from './interfaces/chat-message';
+export * from './interfaces/chat-task-manager';
 export default plugin;
