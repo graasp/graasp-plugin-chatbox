1f3538c21d60d0a3d4375cf166cffbcd05c2680e
pyphilia <pyphilia@gmail.com>
2021-10-13 10:36:25 +0200

    refactor: match packages with core

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import plugin from './service-api';
-
+export { default } from './service-api';
 export * from './db-service';
 export * from './interfaces/chat';
 export * from './interfaces/chat-message';
 export * from './interfaces/chat-task-manager';
-export default plugin;
