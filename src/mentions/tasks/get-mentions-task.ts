0b8378e8ebe79aba5bfd8f030b0faf675e00aeb3
spaenleh <basile.1358@gmail.com>
2022-07-16 20:04:45 +0200

    test: get user mentions

--- src/mentions/tasks/get-mentions-task.ts
+++ src/mentions/tasks/get-mentions-task.ts
@@ -14,13 +14,16 @@ export class GetMemberMentionsTask extends BaseMentionTask<ChatMention[]> {
   constructor(member: Actor, mentionService: MentionService) {
     super(member, mentionService);
     this.targetId = member.id;
+    console.log('Member id:', member.id);
   }
 
   async run(handler: DatabaseTransactionHandler): Promise<void> {
     this.status = 'RUNNING';
-
+    console.log('Hello i am getting your mentions');
     // get all mentions
-    this._result = await this.mentionService.getAll(this.targetId, handler);
+    const mentions = await this.mentionService.getAll(this.targetId, handler);
+    console.log(mentions);
+    this._result = mentions;
     this.status = 'OK';
   }
 }
