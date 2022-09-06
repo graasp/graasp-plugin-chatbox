22a274ff33870e94c516303100089e8c1f786e09
spaenleh <spaenleh@gmail.com>
2022-09-06 10:52:47 +0200

    test: log

--- src/mentions/mailer-hooks.ts
+++ src/mentions/mailer-hooks.ts
@@ -47,7 +47,7 @@ export function registerChatMentionsMailerHooks(
       )
       .catch((err) => {
         log.warn(err, `mailer failed. notification link: ${itemLink}`);
-        log.warn(member, 'mentioned by', mentionCreator);
+        console.log(member, 'mentioned by', mentionCreator);
       });
   };
 
